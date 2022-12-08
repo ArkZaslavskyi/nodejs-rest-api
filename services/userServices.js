611b6e8f6cae15b437d5ac85c5afab13aaedb3b6
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-08 04:33:42 +0200

    add user services

---
+++ services/userServices.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { User } = require("../models");
+const { requestError } = require("../helpers/apiHelpers");
+
+const signUp = async (email, password) => {
+  const user = new User({ email, password });
+  const signedUser = await user.save();
+  return signedUser;
+};
+
+const login = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw requestError(401, "Email or password is wrong"); // `Email ${email} is wrong`
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    throw requestError(401, "Email or password is wrong"); // 'Password is wrong'
+  }
+
+  const { JWT_SECRET: secret } = process.env;
+
+  const token = jwt.sign({ _id: user._id, createdAt: user.createdAt }, secret); //, { expiresIn: "15m" }
+
+  await User.findByIdAndUpdate(
+    user._id,
+    { $set: { token } },
+    { returnDocument: "after" }
+  );
+
+  return { user, token };
+};
+
+const logout = async (user) => {
+  const { _id } = user;
+  return await User.findByIdAndUpdate(_id, { $set: { token: null } });
+};
+
+module.exports = {
+  signUp,
+  login,
+  logout,
+};
