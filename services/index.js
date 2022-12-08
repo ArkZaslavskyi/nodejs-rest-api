53734f723fb8db077b61833b654b700c76d64dc9
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-08 04:29:34 +0200

    refactoring contacts services

--- services/index.js
+++ services/index.js
@@ -1,42 +1,20 @@
-const { Contact } = require("../models");
+const {
+  getContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  removeContactById,
+  updateStatusContactById,
+} = require("./contactsServices");
 
-const getContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
-};
-
-const getContactById = async (id) => {
-  const contact = await Contact.findById(id);
-  return contact;
-};
-
-const addContact = async (body) => {
-  const contact = new Contact(body);
-  const postedContact = await contact.save();
-  return postedContact;
-};
-
-const updateContactById = async (id, body) => {
-  const contact = await Contact.findByIdAndUpdate(
-    id,
-    { $set: body },
-    { returnDocument: "after" }
-  );
-
-  return contact;
-};
-
-const removeContactById = async (id) => {
-  const deletedContact = await Contact.findByIdAndDelete(id);
-
-  return deletedContact;
-};
-
-const updateStatusContactById = async (id, body) => {
-  return updateContactById(id, body);
-};
+const { signUp, login, logout } = require("./userServices");
 
 module.exports = {
+  //
+  signUp,
+  login,
+  logout,
+  //
   getContacts,
   getContactById,
   addContact,
