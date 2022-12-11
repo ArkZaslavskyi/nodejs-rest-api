2d44b3719663f91d7537566651eb172446584e16
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-12-11 18:51:24 +0200

    refactoring authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,13 +1,20 @@
+/**
+ *  1. Извлекаем из заголовка 'authorization'.
+ *  2. Делим 'authorization' на 'tokenType' и 'token'.
+ *  3. Проверям 'tokenType === "Bearer"' и наличие 'token'.
+ *  4. Декодируем с помощью 'token' и 'JWT_secret' в 'decodedUser'.
+ *  5. Проверяем по 'decodedUser._id' наличие в базе декодированного user.
+ *  6. Если 'user' в базе найден, и у него есть 'token' (проверяем это),
+ *     прикрепляем 'user' и 'token' к объекту запроса (записываем  в 'req'),
+ *     "пробрасывая" таким образом для дальнейшего использования в контроллерах.
+ */
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { requestError } = require("../helpers/apiHelpers");
+const { User } = require("../models");
 
-const authMiddleware = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization) {
-    next(requestError(401, "Not authorized"));
-  }
+const authMiddleware = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
 
   const [tokenType, token] = authorization.split(" ");
 
@@ -18,12 +25,26 @@ const authMiddleware = (req, res, next) => {
   const { JWT_SECRET: secret } = process.env;
 
   try {
-    const user = jwt.decode(token, secret);
+    const decodedUser = jwt.verify(token, secret);
+    const user = await User.findById(decodedUser._id);
+
+    if (!user || !user.token) {
+      next(requestError(401, "Not authorized"));
+    }
+
     req.user = user; // pass 'user' to next middlewares
     req.token = token; // pass 'token' to next middlewares
+
     next();
   } catch (error) {
-    next(requestError(401, "Not authorized"));
+    if (
+      error.name === "JsonWebTokenError" ||
+      error.name === "TokenExpiredError"
+    ) {
+      error.status = 401;
+      error.message = "Not authorized";
+    }
+    next(error);
   }
 };
 
