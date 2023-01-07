577d60cfb03289540c5de92a894ef8e10e389458
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-01-08 01:15:16 +0200

    files router - deleted comments

--- routes/api/files.js
+++ routes/api/files.js
@@ -5,22 +5,5 @@ const router = new express.Router();
 const avatarDir = path.resolve(__dirname, "../../public", "avatars");
 
 router.use("/avatars", express.static(avatarDir));
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, fileDir);
-//   },
-//   filename: (req, file, cb) => {
-//     const [, extension] = file.originalname.split(".");
-//     cb(null, `${uuidv4()}.${extension}`);
-//   },
-// });
-
-// const uploadMiddleware = multer({ storage });
-
-// router.post(
-//   "/upload",
-//   uploadMiddleware.single("avatar"),
-//   asyncWrapper(ctrl.upload)
-// );
 
 module.exports = router;
