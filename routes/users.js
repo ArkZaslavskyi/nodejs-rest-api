9356bd387de22f943dec18a4b7ff591144c3419b
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-01-08 01:22:22 +0200

    users/avatars route added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
-
 const ctrl = require("../controllers");
 
 const { asyncWrapper } = require("../helpers/apiHelpers");
-const { validationBody, isValidId, authMiddleware } = require("../middlewares");
+const {
+  validationBody,
+  isValidId,
+  authMiddleware,
+  uploadMiddleware,
+} = require("../middlewares");
 
 const { schemaUser, schemaUserSubscription } = require("../schemes");
 
@@ -13,6 +17,32 @@ router.post("/signup", validationBody(schemaUser), asyncWrapper(ctrl.signUp));
 router.post("/login", validationBody(schemaUser), asyncWrapper(ctrl.login));
 router.get("/logout", authMiddleware, asyncWrapper(ctrl.logout));
 router.get("/current", authMiddleware, asyncWrapper(ctrl.current));
+
+// const multer = require("multer");
+// const path = require("path");
+// const { v4: uuidv4 } = require("uuid");
+
+// const tmpDir = path.resolve(__dirname, "../tmp");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     console.log(tmpDir);
+//     cb(null, tmpDir);
+//   },
+//   filename: (req, file, cb) => {
+//     const [, ext] = file.originalname.split(".");
+//     cb(null, `${uuidv4()}.${ext}`);
+//   },
+// });
+// const uploadMiddleware = multer({ storage });
+
+router.patch(
+  "/avatars",
+  authMiddleware,
+  uploadMiddleware.single("avatar"),
+  asyncWrapper(ctrl.patchUserAvatar)
+);
+
 router.patch(
   "/:id",
   isValidId,
